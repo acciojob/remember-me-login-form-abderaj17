43e456a054fa6bb3f47a7e5b2c91ab99c6e2009d
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2024-11-15 21:31:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,29 +4,30 @@ const passwordField = document.querySelector("#password");
 const checkbox = document.querySelector("#checkbox");
 const submitBtn = document.querySelector("#submit");
 
-
 submitBtn.addEventListener('click', ()=>{
 	
-const username = usernameField.value;
-const password = passwordField.value;
-	if(checkbox.checked){
-		localStorage.setItem("username", username)
-		localStorage.setItem("password", password);
-	} else if(!checkbox.checked){
-		localStorage.removeItem("username")
-		localStorage.removeItem("password")
-		alert("Logged in as "+ username)
-	}
+  const username = usernameField.value;
+  const password = passwordField.value;
+
+  if(checkbox.checked){
+    localStorage.setItem("username", username)
+    localStorage.setItem("password", password);
+  } else {
+    localStorage.removeItem("username")
+    localStorage.removeItem("password")
+  }
+
+  alert("Logged in as "+ username)
 })
 
 if(localStorage.getItem("username")&& localStorage.getItem("password")){
-	const existingUserBtn = document.createElement("button");
-	existingUserBtn.textContent = "Login as existing user";
-	existingUserBtn.id="existing";
-	document.body.appendChild(existingUserBtn);
+  const existingUserBtn = document.createElement("button");
+  existingUserBtn.textContent = "Login as existing user";
+  existingUserBtn.id="existing";
+  document.body.appendChild(existingUserBtn);
 
-	existingUserBtn.addEventListener('click', ()=>{
-		const username = localStorage.getItem("username");
-		alert("Logged in as "+ username);
-	})
+  existingUserBtn.addEventListener('click', ()=>{
+    const username = localStorage.getItem("username");
+    alert("Logged in as "+ username);
+  })
 }
